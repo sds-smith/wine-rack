3cde85d555246cebf8934678b6626da58b897583
Shawn Smith <sds.smith24@gmail.com>
2025-02-15 13:37:35 -0500

    call db from getWines

--- app/components/PrintButton.tsx
+++ app/components/PrintButton.tsx
@@ -3,15 +3,15 @@
 import React from 'react';
 import IconButton from '@mui/material/IconButton';
 import PrintIcon from '@mui/icons-material/Print';
-import Link from 'next/link';
+// import Link from 'next/link';
 
 export default function PrintButton() {
 
-    return (
-      <Link href={`/print`}>
-        <IconButton aria-label="print" size='small'>
-            <PrintIcon />
-        </IconButton>
-      </Link>
-    )
+  return (
+    // <Link href={`/print`}>
+      <IconButton aria-label="print" size='small'>
+          <PrintIcon />
+      </IconButton>
+    // </Link>
+  )
 }
