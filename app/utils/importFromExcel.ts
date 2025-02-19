8483e12ece6f5fce99bcc4ab132a6f2e332a36b6
Shawn Smith <sds.smith24@gmail.com>
2025-02-19 17:48:15 -0500

    updates

--- app/utils/importFromExcel.ts
+++ app/utils/importFromExcel.ts
@@ -1,6 +1,6 @@
 
 import XLSX from 'xlsx';
-import { Wine, defaultWineState, Ready } from '../types/wine';
+import { Wine, defaultWineState, Ready, WineField } from '../types/wine';
 
 type RawData = {
     A: string,
@@ -43,34 +43,34 @@ type Columns = {
 export async function importFromExcel() {
     const url = "https://docs.google.com/spreadsheets/d/1RQ3LxmN14ulIhLdy3R6ddqvOj3Prlvv2/edit?usp=sharing&ouid=113315703908196041103&rtpof=true&sd=true";
     const file = await (await fetch(url)).arrayBuffer();
-    const workbook = XLSX.read(file);
+    const workbook: XLSX.WorkBook = XLSX.read(file);
 
-    const { Sheet1 } = workbook.Sheets;
+    const Sheet1 = workbook.Sheets.Sheet1;
 
     const raw_data: RawData[] = XLSX.utils.sheet_to_json(Sheet1);
 
     const columns: Columns = {
-        A: {heading: "Category",    convert: (value:string): string => `${value}`.replaceAll('&#39;', "'")}, 
-        B: {heading: "Varietal",    convert: (value:string): string => `${value}`.replaceAll('&#39;', "'")}, 
-        C: {heading: "Country",     convert: (value:string): string => `${value}`.replaceAll('&#39;', "'")}, 
-        D: {heading: "Vintage",     convert: (value:string): string => `${value}`.replaceAll('&#39;', "'")}, 
-        E: {heading: "Producer",    convert: (value:string): string => `${value}`.replaceAll('&#39;', "'")}, 
-        F: {heading: "Label",       convert: (value:string): string => `${value}`.replaceAll('&#39;', "'")}, 
-        G: {heading: "Appellation", convert: (value:string): string => `${value}`.replaceAll('&#39;', "'")}, 
-        H: {heading: "Ready",       convert: (value:string): Ready => ({open: value.split('-')[0], close: value.split('-')[1]})}, 
-        I: {heading: "Source",      convert: (value:string): string => `${value}`.replaceAll('&#39;', "'")}, 
-        J: {heading: "Price",       convert: (value: number): number => value}, 
-        K: {heading: "Acquired",    convert: (value:string): string => `${value}`.replaceAll('&#39;', "'")}, 
-        L: {heading: "Notes",       convert: (value:string): boolean => value === 'x' || value === 'X'}, 
-        M: {heading: "Quantity",    convert: (value:number): number => value}, 
-        N: {heading: "Comments",    convert: (value:string): string => `${value}`.replaceAll('&#39;', "'")}, 
+        A: {heading: "Category",    convert(value:string): string  {return`${value}`.replaceAll('&#39;', "'")}}, 
+        B: {heading: "Varietal",    convert(value:string): string  {return`${value}`.replaceAll('&#39;', "'")}}, 
+        C: {heading: "Country",     convert(value:string): string  {return`${value}`.replaceAll('&#39;', "'")}}, 
+        D: {heading: "Vintage",     convert(value:string): string  {return`${value}`.replaceAll('&#39;', "'")}}, 
+        E: {heading: "Producer",    convert(value:string): string  {return`${value}`.replaceAll('&#39;', "'")}}, 
+        F: {heading: "Label",       convert(value:string): string  {return`${value}`.replaceAll('&#39;', "'")}}, 
+        G: {heading: "Appellation", convert(value:string): string  {return`${value}`.replaceAll('&#39;', "'")}}, 
+        H: {heading: "Ready",       convert(value:string): Ready   {return({open: value.split('-')[0], close: value.split('-')[1]})}}, 
+        I: {heading: "Source",      convert(value:string): string  {return`${value}`.replaceAll('&#39;', "'")}}, 
+        J: {heading: "Price",       convert(value:number): number  {return value}}, 
+        K: {heading: "Acquired",    convert(value:string): string  {return`${value}`.replaceAll('&#39;', "'")}}, 
+        L: {heading: "Notes",       convert(value:string): boolean {return value === 'x' || value === 'X'}}, 
+        M: {heading: "Quantity",    convert(value:number): number  {return value}}, 
+        N: {heading: "Comments",    convert(value:string): string  {return`${value}`.replaceAll('&#39;', "'")}}, 
     }
 
-    const wines: Wine[] = raw_data.map((row:RawData) => {
-        return Object.entries(columns).reduce((acc, [key, {heading, convert}]) => ({
+    const wines: Wine[] = raw_data.map((row:RawData) => 
+        Object.entries(columns).reduce( (acc, [key, {heading, convert}]): Wine => ({
             ...acc,
-            ...(Boolean(row[key as keyof RawData]) && { [heading as keyof Wine]: convert(row[key as keyof RawData]) })
-        }), defaultWineState)
-    })
+            ...(Boolean(row[key as keyof RawData]) && { [heading as keyof Wine]: convert(row[key as keyof RawData]) as WineField })
+        }), defaultWineState))
+    
     return wines
 }
