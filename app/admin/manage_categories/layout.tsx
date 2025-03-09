2a1bca639481abc751c307f6e51a28f155b04037
Shawn Smith <sds.smith24@gmail.com>
2025-03-09 14:16:12 -0400

    develop manage_categories/layout

---
+++ app/admin/manage_categories/layout.tsx
@@ -0,0 +1,23 @@
+
+import { redirect } from 'next/navigation';
+import Container from '@mui/material/Container';
+import { auth, checkFirstLogin } from "@/auth"
+import styles from "../../page.module.css";
+
+export default async function ManageCategories({ children }: { children: React.ReactNode }) {
+  const session = await auth();
+  const email = session?.user?.email;
+  const firstLogin = await checkFirstLogin(email!);
+
+  if (firstLogin) return redirect('/login/change_password')
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h3 >Manage Categories</h3>
+        <Container disableGutters sx={{display: 'flex', flexWrap: 'wrap', marginTop:'20px'}}>
+          { children }
+        </Container>
+      </main>
+    </div>
+  );
+}
--- app/admin/manage_categories/new/page.tsx
+++ app/admin/manage_categories/new/page.tsx
@@ -1,26 +1,12 @@
 
-import { redirect } from 'next/navigation';
-import Container from '@mui/material/Container';
 import CategoriesTable from '../../../components/admin/CategoriesTable';
 import AddCategoryForm from '@/app/components/admin/AddCategoryForm';
-import { auth, checkFirstLogin } from "@/auth"
-import styles from "../../../page.module.css";
 
 export default async function NewCategoryPage() {
-  const session = await auth();
-  const email = session?.user?.email;
-  const firstLogin = await checkFirstLogin(email!);
-
-  if (firstLogin) return redirect('/login/change_password')
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h3 >Manage Categories</h3>
-        <Container disableGutters sx={{display: 'flex', flexWrap: 'wrap', marginTop:'20px'}}>
-          <CategoriesTable disabled={true} />
-          <AddCategoryForm />
-        </Container>
-      </main>
-    </div>
+    <>
+      <CategoriesTable disabled={true} />
+      <AddCategoryForm />
+    </>
   );
 }
