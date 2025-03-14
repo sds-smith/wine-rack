a210b743a266ca923d1f38b8b20fe1885d5e3d1d
Shawn Smith <sds.smith24@gmail.com>
2025-03-14 17:41:33 -0400

    clean up mobile search form

--- app/components/mobile/MobileSearchForm.tsx
+++ app/components/mobile/MobileSearchForm.tsx
@@ -2,11 +2,13 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 import CategorySelect from "../controlled_inputs/CategorySelect";
-import { Category, Wine } from "@/app/types/wine";
 import ProducerSelect from "../controlled_inputs/ProducerSelect";
 import WineSelect from "../controlled_inputs/WineSelect";
-import Button from "@mui/material/Button";
+import { Category, Wine } from "@/app/types/wine";
+
 
 const defaultCategory = { ID: '', code: ' ', title: 'Please select category', group: ''};
 
@@ -31,16 +33,20 @@ export default function MobileSearchForm({ categories, wineList } : MobileSearch
     const producers: string[] = [ ...new Set(wineList.filter(categoryFilter).map(({ Producer }: Wine) => Producer).sort())]
 
     const handleSelectCategory = (newSelected: Category) => {
-        setSelectedCategory(newSelected)
-        setSelectedProducer('')
+        setSelectedCategory(newSelected);
+        setSelectedProducer('');
+        setSelectedWine(undefined)
     };
-    const handleSelectProducer = (newSelected: string) => setSelectedProducer(newSelected);
+    const handleSelectProducer = (newSelected: string) => {
+        setSelectedProducer(newSelected);
+        setSelectedWine(undefined)
+    };
     const handleSelectWine = (newSelected: Wine) => setSelectedWine(newSelected)
 
     const handleSearch = () => router.push(`/mobile/view/${selectedWine?.ID}`)
 
     return (
-        <>
+        <Container sx={{minHeight: '60vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}>
             <h2>Search Current Inventory</h2>
             <CategorySelect 
                 categories={categories} 
@@ -61,12 +67,13 @@ export default function MobileSearchForm({ categories, wineList } : MobileSearch
                 selectedWine={selectedWine}
             />
             <Button 
+                fullWidth
                 disabled={!selectedWine} 
                 variant={'contained'}
                 onClick={handleSearch}
             >
                 Select
             </Button>
-        </> 
+        </Container> 
     )
 }
