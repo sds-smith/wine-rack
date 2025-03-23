4b6b01a0c2f04d62df09a2ca5e266b43941909bd
Shawn Smith <sds.smith24@gmail.com>
2025-03-23 07:26:41 -0400

    test params current inv

--- app/dashboard/current_inventory/page.tsx
+++ app/dashboard/current_inventory/page.tsx
@@ -1,20 +1,21 @@
 
-// import TablePage from "@/app/components/table/TablePage";
+import TablePage from "@/app/components/table/TablePage";
 
-// type PageProps = { 
-//   searchParams?: Promise<{ 
-//     filter_by_category?: string 
-//     order?: string 
-//     orderBy?: string 
+type PageProps = { 
+  searchParams?: Promise<{ 
+    filter_by_category?: string 
+    order?: string 
+    orderBy?: string 
     
-//   }>, 
-// }
+  }>, 
+}
 
-export default async function Rack(/*props: PageProps*/) {
-  // const searchParams = await props.searchParams;
-  // const filter_by_category = searchParams?.filter_by_category || '';
+export default async function Rack(props: PageProps) {
+  const searchParams = await props.searchParams;
+  const filter_by_category = searchParams?.filter_by_category || '';
   // const order = searchParams?.order || '';
   // const orderBy = searchParams?.orderBy || '';
+  console.log({filter_by_category})
 
   return (
     <div>current inventory</div>
