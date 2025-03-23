12436081dd1e2de2e4c723a6561f846e53217166
Shawn Smith <sds.smith24@gmail.com>
2025-03-23 07:22:39 -0400

    test current inventory page

--- app/dashboard/current_inventory/page.tsx
+++ app/dashboard/current_inventory/page.tsx
@@ -1,25 +1,26 @@
 
-import TablePage from "@/app/components/table/TablePage";
+// import TablePage from "@/app/components/table/TablePage";
 
-type PageProps = { 
-  searchParams?: Promise<{ 
-    filter_by_category?: string 
-    order?: string 
-    orderBy?: string 
+// type PageProps = { 
+//   searchParams?: Promise<{ 
+//     filter_by_category?: string 
+//     order?: string 
+//     orderBy?: string 
     
-  }>, 
-}
+//   }>, 
+// }
 
-export default async function Rack(props: PageProps) {
-  const searchParams = await props.searchParams;
-  const filter_by_category = searchParams?.filter_by_category || '';
-  const order = searchParams?.order || '';
-  const orderBy = searchParams?.orderBy || '';
+export default async function Rack(/*props: PageProps*/) {
+  // const searchParams = await props.searchParams;
+  // const filter_by_category = searchParams?.filter_by_category || '';
+  // const order = searchParams?.order || '';
+  // const orderBy = searchParams?.orderBy || '';
 
   return (
-    <TablePage 
-      page={'current_inventory'}
-      searchParams={{filter_by_category, order, orderBy}}
-    />
+    <div>current inventory</div>
+    // <TablePage 
+    //   page={'current_inventory'}
+    //   searchParams={{filter_by_category, order, orderBy}}
+    // />
   );
 }
