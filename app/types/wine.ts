355e97dd098abb1a014a47d86c795f843015ddc3
Shawn Smith <sds.smith24@gmail.com>
2025-05-03 07:42:58 -0400

    clean up types

--- app/types/wine.ts
+++ app/types/wine.ts
@@ -8,11 +8,11 @@ export interface Category {
     color?: string
 }
 
-export type CategoriesByCode = {
+export interface CategoriesByCode {
     [key: string] : Category
 }
 
-export type Ready = {
+export interface Ready {
     open?    : string;
     close?   : string;
 }
@@ -37,54 +37,15 @@ export interface Wine extends WithId<Document> {
     GetMore?    : boolean;
 };
 
-export type WineInput = {
-    ID?         : string;
-    Category    : string;
-    Varietal?   : string;
-    Country?    : string;
-    Vintage?    : string;
-    Producer    : string;
-    Label?      : string;
-    Appellation?: string;
-    Ready       : Ready;
-    Source?     : string;
-    Price?      : string;
-    Acquired?   : string;
-    Notes       : string;
-    Quantity    : string;
-    Comments?   : string;
-    Archived?   : string;
-    GetMore?    : string;
-};
-
 export type WineField = string | number | boolean | Ready | null | undefined;
 
-export type Columns = {
-    A: string,
-    B: string,
-    C: string,
-    D: string,
-    E: string,
-    F: string,
-    G: string,
-    H: string,
-    I: string,
-    J: string,
-    K: string,
-    L: string,
-    M: string,
-    N: string,
-    O: string,
-}
-
-export type Metadata = {
+export interface Metadata {
     totalBottles: number
 }
 
 export const initialMetaData = {
     totalBottles: 0
 }
-
 
 export type Column = "Category" | "Varietal" | "Country" | "Vintage" | "Producer" | "Label" 
     | "Appellation" | "Ready" | "Source" | "Price" | "Acquired" | "Notes" | "Quantity" | "Comments" 
