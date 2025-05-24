8e6c0249479528595a3bbc6f0e2d09f770205207
Shawn Smith <sds.smith24@gmail.com>
2025-05-24 08:33:47 -0400

    remove current print solution

--- app/components/buttons/PrintButton.tsx
+++ app/components/buttons/PrintButton.tsx
@@ -1,26 +1,19 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
 import IconButton from '@mui/material/IconButton';
 import PrintIcon from '@mui/icons-material/Print';
-import Link from 'next/link';
 import { useResponsive } from '@/app/hooks/useResponsive';
 
 export default function PrintButton() {
-  const pathname = usePathname();
-  const path = pathname.split('/')
-  const page = path[path.length - 1]
 
   const { isDesktop } = useResponsive();
 
   return (
     <>
       { isDesktop
-        ? <Link href={`/print/${page}`}>
-            <IconButton aria-label="print" size='small'>
-              <PrintIcon />
-            </IconButton>
-          </Link>
+        ? <IconButton aria-label="print" size='small'>
+            <PrintIcon />
+          </IconButton>
         : null
       }
     </>
