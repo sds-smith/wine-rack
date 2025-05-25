726a217e5c3bcac03cc1c3e01a69efce98c0ebd8
Shawn Smith <sds.smith24@gmail.com>
2025-05-25 10:58:42 -0400

    develop makePdf and pdf route handler

--- app/api/pdf/route.ts
+++ app/api/pdf/route.ts
@@ -1,15 +1,17 @@
-import { getWineData, columns } from "../../utils/data";
+import { getWineData, getCategories, columns } from "../../utils/data";
 
 export async function GET(request: Request) {
   const url = new URL(request.url)
   const searchParams = new URLSearchParams(url.search);
   const page = searchParams.get("page");
-  console.log({page})
 
   const { wineList } = await getWineData(page || 'current_inventory')
+  const { categoriesByCode } = await getCategories()
+  columns[columns.indexOf('Quantity')] = 'Qty'
 
   return Response.json({
     wineList,
-    columnHeadings: columns.slice(1)
+    columnHeadings: columns.slice(1),
+    categoriesByCode
   })
 }
--- app/utils/pdf.ts
+++ app/utils/pdf.ts
@@ -2,9 +2,10 @@
 import pdfMake from "pdfmake/build/pdfmake";
 import "pdfmake/build/vfs_fonts";
 import { PageOrientation } from "pdfmake/interfaces";
-import { Wine } from "../types/wine";
+import { Wine, CategoriesByCode } from "../types/wine";
 
-// pdfMake.vfs = pdfFonts.pdfMake.vfs;
+type Cell = string | { text: string, style: {fillColor: string | undefined}}
+
 pdfMake.fonts = {
   'Roboto': {
      normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf',
@@ -14,17 +15,29 @@ pdfMake.fonts = {
   }
 };
 
-function getDocDef(columnHeadings: string[], rows: string[][]) {
+function getDocDef(page: string, columnHeadings: string[], rows: Cell[][]) {
+  const pageTitle = {
+    text: page.split('_').map(word => word.toLocaleUpperCase()).join(' '),
+    fontSize: 14, bold: true, marginBottom: 10
+  };
   return {
     pageOrientation: 'landscape' as PageOrientation,
+    defaultStyle: {
+      fontSize: 10
+    },
+    footer: ((currentPage: { toString: () => string; }, pageCount: string) =>  ({
+      text:`${page} ${currentPage.toString()} of ${pageCount}`,
+      alignment: 'right',
+      marginRight: 30
+    })),
     content: [
+      pageTitle,
       {
-        layout: 'lightHorizontalLines', // optional
+        layout: 'lightHorizontalLines',
         table: {
-          // headers are automatically repeated if the table spans over multiple pages
-          // you can declare how many rows should be treated as headers
           headerRows: 1,
-
+          dontBreakRows: true,
+          widths: [ 55,  35,  35,  55,  55,  55,  35,  55,  35,  55,  30,  30,  60, ],
           body: [
             columnHeadings,
             ...rows
@@ -35,26 +48,37 @@ function getDocDef(columnHeadings: string[], rows: string[][]) {
   };
 }
 
+function buildRows(data: Wine[], categoriesByCode: CategoriesByCode): Cell[][] {
+  const spacerRow = Array(13).fill({text: '', style:{fillColor: '#eeeeee', lineHeight: 8}},)
+  const footerRow = ['','','','','','','','','','','Total:',`${data.reduce((a, c)=>a+c.Quantity, 0)}`, '']
+  const getFillColor = (wine: Wine) => categoriesByCode[wine.Category].color;
+  const rows = data.flatMap((wine: Wine, idx: number) => {
+    const row =  Object.values({
+      "Varietal": { text: `${wine.Varietal || ''}`, style: { fillColor: getFillColor(wine) }},
+      "Country": `${wine.Country || ''}`,
+      "Vintage": `${wine.Vintage || ''}`,
+      "Producer": `${wine.Producer || ''}`,
+      "Label": `${wine.Label || ''}`,
+      "Appellation": `${wine.Appellation || ''}`,
+      "Ready": `${!wine.Ready.close || wine.Ready.close === wine.Ready.open ? `${wine.Ready.open }` : `${wine.Ready.open} - ${wine.Ready.close}`}`,
+      "Source": `${wine.Source || ''}`,
+      "Price": `${wine.Price || ''}`,
+      "Acquired": `${wine.Acquired || ''}`,
+      "Notes": `${wine.Notes ? 'x' : ''}`,
+      "Quantity": `${wine.Quantity}`,
+      "Comments": `${wine.Comments || ''}`,
+    })
+    return (idx > 0 && wine.Category !== data[idx-1]?.Category) ? [spacerRow, row] :  [row]
+  })
+  rows.push(footerRow)
+  return rows
+}
+
 export const makePdf = async (page: string) => {
   const response= await fetch(`/api/pdf?page=${page}`)
-  const { wineList, columnHeadings} = await response.json()
+  const { wineList, columnHeadings, categoriesByCode } = await response.json()
 
-  const rows: string[][] = wineList.map((wine: Wine) => Object.values({
-    "Varietal": `${wine.Varietal}`,
-    "Country": `${wine.Country}`,
-    "Vintage": `${wine.Vintage}`,
-    "Producer": `${wine.Producer}`,
-    "Label": `${wine.Label}`,
-    "Appellation": `${wine.Appellation}`,
-    "Ready": `${wine.Ready.close || wine.Ready.close === wine.Ready.open ? `${wine.Ready.open }` : `${wine.Ready.open} - ${wine.Ready.close}`}`,
-    "Source": `${wine.Source}`,
-    "Price": `${wine.Price}`,
-    "Acquired": `${wine.Acquired}`,
-    "Notes": `${wine.Notes}`,
-    "Quantity": `${wine.Quantity}`,
-    "Comments": `${wine.Comments}`,
-  }))
-
-  const docDefinition = getDocDef(columnHeadings, rows)
+  const rows = buildRows(wineList, categoriesByCode)
+  const docDefinition = getDocDef(page, columnHeadings, rows)
   pdfMake.createPdf(docDefinition).print()
 };
