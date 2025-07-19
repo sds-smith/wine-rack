c1fbb7a3ba3d4b6011f84e7cb701ffc20dcbcbfd
Shawn Smith <sds.smith24@gmail.com>
2025-07-19 11:23:37 -0400

    page as Page

--- app/components/buttons/PrintButton.tsx
+++ app/components/buttons/PrintButton.tsx
@@ -5,11 +5,12 @@ import IconButton from '@mui/material/IconButton';
 import PrintIcon from '@mui/icons-material/Print';
 import { useResponsive } from '@/app/hooks/useResponsive';
 import { generatePdf } from '@/app/utils/pdf';
+import { Page } from '@/app/utils/data';
 
 export default function PrintButton() {
   const pathname = usePathname();
   const path = pathname.split('/')
-  const page = path[path.length - 1]
+  const page = path[path.length - 1] as Page
 
   const { isDesktop } = useResponsive();
 
